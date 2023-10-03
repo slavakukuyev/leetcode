55d264b1ca07fd930bf4dca8eac8c35bd7124183
Slava Kukuyev <slava.kukuyev@gmail.com>
2023-10-03 11:54:03 +0300

    typo

--- indexes_of_sum/indexes_of_sum.ts
+++ indexes_of_sum/indexes_of_sum.ts
@@ -19,7 +19,7 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-//lets assume we dont wont to use the same element twice
+//lets assume we dont want to use the same element twice
 function twoSum(nums: Array<number>, target: number): Array<number> {
     if (!Array.isArray(nums) || nums.length < 2) {
         return []
