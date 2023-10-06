35b1459077038904d3ab7249a44c256c918845d7
Slava Kukuyev <slava.kukuyev@gmail.com>
2023-10-06 11:05:02 +0300

    format files

--- reverse_integer/reverse_integer.test.ts
+++ reverse_integer/reverse_integer.test.ts
@@ -1,68 +1,53 @@
 import { reverseInteger } from './reverse_integer';
 
 describe('reverse', () => {
-    // Should reverse a positive number
-    it('should reverse a positive number when input is positive', () => {
-      expect(reverseInteger(123)).toBe(321);
-    });
+  // Should reverse a positive number
+  it('should reverse a positive number when input is positive', () => {
+    expect(reverseInteger(123)).toBe(321);
+  });
 
-    // Should reverse a negative number
-    it('should reverse a negative number when input is negative', () => {
-      expect(reverseInteger(-123)).toBe(-321);
-    });
+  // Should reverse a negative number
+  it('should reverse a negative number when input is negative', () => {
+    expect(reverseInteger(-123)).toBe(-321);
+  });
 
-    // Should reverse a number with trailing zeros
-    it('should reverse a number with trailing zeros when input has trailing zeros', () => {
-      expect(reverseInteger(1200)).toBe(21);
-    });
+  // Should reverse a number with trailing zeros
+  it('should reverse a number with trailing zeros when input has trailing zeros', () => {
+    expect(reverseInteger(1200)).toBe(21);
+  });
 
-    // Should return 0 when input is 0
-    it('should return 0 when input is 0', () => {
-      expect(reverseInteger(0)).toBe(0);
-    });
+  // Should return 0 when input is 0
+  it('should return 0 when input is 0', () => {
+    expect(reverseInteger(0)).toBe(0);
+  });
 
-    // Should return 0 when reversed number is outside of the 32-bit signed integer range
-    it('should return 0 when reversed number is outside of the 32-bit signed integer range', () => {
-      expect(reverseInteger(2147483648)).toBe(0);
-    });
+  // Should return 0 when reversed number is outside of the 32-bit signed integer range
+  it('should return 0 when reversed number is outside of the 32-bit signed integer range', () => {
+    expect(reverseInteger(2147483648)).toBe(0);
+  });
 
-    // Should return 0 when input is outside of the 32-bit signed integer range
-    it('should return 0 when input is outside of the 32-bit signed integer range', () => {
-      expect(reverseInteger(2147483649)).toBe(0);
-    });
+  // Should return 0 when input is outside of the 32-bit signed integer range
+  it('should return 0 when input is outside of the 32-bit signed integer range', () => {
+    expect(reverseInteger(2147483649)).toBe(0);
+  });
 
-    // Should reverse a single digit number
-    it('should reverse a single digit number when input is a single digit number', () => {
-      expect(reverseInteger(5)).toBe(5);
-    });
+  // Should reverse a single digit number
+  it('should reverse a single digit number when input is a single digit number', () => {
+    expect(reverseInteger(5)).toBe(5);
+  });
 
-    // Should handle the largest negative number (-2^31) correctly
-    it('should handle the largest negative number correctly when input is the largest negative number', () => {
-      expect(reverseInteger(-2147483648)).toBe(0);
-    });
+  // Should handle the largest negative number (-2^31) correctly
+  it('should handle the largest negative number correctly when input is the largest negative number', () => {
+    expect(reverseInteger(-2147483648)).toBe(0);
+  });
 
-    // Should return 0 when input is a floating-point number
-    it('should return 0 when input is a floating-point number', () => {
-      expect(reverseInteger(3.14)).toBe(0);
-    });
+  // Should return 0 when input is a non-integer number
+  it('should return 0 when input is a non-integer number', () => {
+    expect(reverseInteger(-3.14)).toBe(0);
+  });
 
-    // Should return 0 when input is a non-integer number
-
-
-    // Should return 0 when input is a non-integer number
-    it('should return 0 when input is a non-integer number', () => {
-      expect(reverseInteger(-3.14)).toBe(0);
-    });
-
-    // Should return 0 when input is a non-integer number
-
-
-    // Should return 0 when input is a non-integer number
-    it('should return 0 when input is a non-integer number', () => {
-      expect(reverseInteger(3.14)).toBe(0);
-    });
-
-
-
-
+  // Should return 0 when input is a non-integer number
+  it('should return 0 when input is a non-integer number', () => {
+    expect(reverseInteger(3.14)).toBe(0);
+  });
 });
--- reverse_integer/reverse_integer.ts
+++ reverse_integer/reverse_integer.ts
@@ -1,32 +1,31 @@
 const MIN_NUM = (-2) ** 31
-const MAX_NUM =  2 ** 31 - 1
+const MAX_NUM = 2 ** 31 - 1
 
 function reverseInteger(x: number): number {
+  if (!x || !Number.isInteger(x) || x < MIN_NUM || x > MAX_NUM) return 0
 
-  if(!x || !Number.isInteger(x) || x < MIN_NUM  || x > MAX_NUM) return 0
-
-let sign = 1
+  let sign = 1
   if (x < 0) {
-      sign = -1
-      x = x*-1
+    sign = -1
+    x = x * -1
   }
 
   let arr = ("" + x).split('');
   let left = 0
   let right = arr.length - 1;
 
-  while(left < right) {
+  while (left < right) {
     [arr[left], arr[right]] = [arr[right], arr[left]]
     left++
     right--
   }
 
   x = parseInt(arr.join('')) * sign
-  if  (x < MIN_NUM  || x > MAX_NUM) return 0
+  if (x < MIN_NUM || x > MAX_NUM) {
+    return 0
+  }
 
   return x
-
 };
 
-
-export {reverseInteger}
+export { reverseInteger }
